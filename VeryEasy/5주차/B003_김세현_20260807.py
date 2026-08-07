08390a65a0d0fd109d9a4405d0d3738f9c90994e
kimsehyun <shkim_0125@naver.com>
2026-08-07 23:20:34 +0900

    solve Sum of Left Leaves

---
+++ VeryEasy/5주차/B003_김세현_20260807.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        total = 0
+
+        if root.left and not root.left.left and not root.left.right:
+            total += root.left.val
+
+        total += self.sumOfLeftLeaves(root.left)
+        total += self.sumOfLeftLeaves(root.right)
+
+        return total
