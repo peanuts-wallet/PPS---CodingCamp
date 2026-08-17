5b4d80c972f5e75f7b0b1e53ac7d0507fbe35eb6
kimsehyun <shkim_0125@naver.com>
2026-08-17 22:36:59 +0900

    solve Minimum Distance Between BST Nodes

---
+++ VeryEasy/6주차/B058_김세현_20260817.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        prev = None
+        answer = float('inf')
+
+        def inorder(node):
+            nonlocal prev, answer
+
+            if not node:
+                return
+
+            inorder(node.left)
+
+            if prev is not None:
+                answer = min(answer, node.val - prev)
+
+            prev = node.val
+
+            inorder(node.right)
+
+        inorder(root)
+
+        return answer
