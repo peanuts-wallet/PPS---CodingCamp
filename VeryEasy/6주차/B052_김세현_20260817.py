f8478ca96d262d65522e49b9f1ffb600f06e66dc
kimsehyun <shkim_0125@naver.com>
2026-08-17 22:36:58 +0900

    solve Binary Tree Tilt

---
+++ VeryEasy/6주차/B052_김세현_20260817.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        total_tilt = 0
+
+        def dfs(node):
+            nonlocal total_tilt
+
+            if not node:
+                return 0
+
+            left_sum = dfs(node.left)
+            right_sum = dfs(node.right)
+
+            total_tilt += abs(left_sum - right_sum)
+
+            return left_sum + right_sum + node.val
+
+        dfs(root)
+
+        return total_tilt
