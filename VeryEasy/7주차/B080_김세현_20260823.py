c7a640e874f9cad1fbea561a18538b3017f19cf3
peanuts-wallet <shkim_0125@naver.com>
2026-08-23 22:27:56 +0900

    solve Counting Bits

---
+++ VeryEasy/7주차/B080_김세현_20260823.py
@@ -0,0 +1,13 @@
+# Counting Bits
+# LeetCode 338
+
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        answer = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            answer[i] = answer[i >> 1] + (i & 1)
+
+        return answer
