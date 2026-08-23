4c33f67a55887c72daebade24e8eef7d4c923a58
peanuts-wallet <shkim_0125@naver.com>
2026-08-23 22:27:05 +0900

    solve Get Maximum in Generated Array

---
+++ VeryEasy/7주차/B078_김세현_20260823.py
@@ -0,0 +1,19 @@
+# Get Maximum in Generated Array
+# LeetCode 1646
+
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        if n == 0:
+            return 0
+
+        nums = [0] * (n + 1)
+        nums[1] = 1
+
+        for i in range(1, n + 1):
+            if 2 * i <= n:
+                nums[2 * i] = nums[i]
+
+            if 2 * i + 1 <= n:
+                nums[2 * i + 1] = nums[i] + nums[i + 1]
+
+        return max(nums)
