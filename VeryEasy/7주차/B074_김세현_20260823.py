81c794f17f7025aa87c22c7d7a602871ed0b3f7a
peanuts-wallet <shkim_0125@naver.com>
2026-08-23 22:26:32 +0900

    solve 콜라츠 추측

---
+++ VeryEasy/7주차/B074_김세현_20260823.py
@@ -0,0 +1,14 @@
+# 콜라츠 추측
+# Programmers
+
+def solution(num):
+    count = 0
+
+    while num != 1 and count < 500:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = num * 3 + 1
+        count += 1
+
+    return count if num == 1 else -1
