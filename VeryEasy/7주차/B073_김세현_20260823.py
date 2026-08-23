cbb33473d10434e99ed2a97eac99e8f5f75b4d59
peanuts-wallet <shkim_0125@naver.com>
2026-08-23 22:26:06 +0900

    solve 멀리 뛰기

---
+++ VeryEasy/7주차/B073_김세현_20260823.py
@@ -0,0 +1,17 @@
+# 멀리 뛰기
+# Programmers
+
+def solution(n):
+    MOD = 1234567
+
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    a, b = 1, 2
+
+    for _ in range(3, n + 1):
+        a, b = b, (a + b) % MOD
+
+    return b
