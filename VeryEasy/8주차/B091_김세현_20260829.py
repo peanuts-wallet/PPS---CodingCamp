fbffa37892fefd69edc48bf552d16ddd13e533b4
kimsehyun <shkim_0125@naver.com>
2026-08-29 02:08:26 +0900

    solve Employees Earning More Than Their Managers

---
+++ VeryEasy/8주차/B091_김세현_20260829.py
@@ -0,0 +1,7 @@
+solution = """
+SELECT employee.name AS Employee
+FROM Employee AS employee
+JOIN Employee AS manager
+    ON employee.managerId = manager.id
+WHERE employee.salary > manager.salary;
+""".strip()
